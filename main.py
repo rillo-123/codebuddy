a79242db8e59ba01a53f143a0ab7a42aa880757d
Rickard Ericson <rickard.ericson@ge.com>
2020-12-15 22:45:21 +0100

    tab control test

--- main.py
+++ main.py
@@ -9,19 +9,30 @@ import PySimpleGUI as sg
 
 sg.theme('DarkAmber')   # Add a touch of color
 # All the stuff inside your window.
-layout = [  [sg.Text('Some text on Row 1')],
-            [sg.Text('Enter something on Row 2'), sg.InputText()],
-            [sg.Text('Enter something on Row 3'), sg.InputText()],
-            [sg.Checkbox('Hello')],
-            [sg.Button('Ok'), sg.Button('Cancel')] ]
+
+
+# layout = [  [sg.Text('Some text on Row 1')],
+#             [sg.Text('Enter something on Row 2'), sg.InputText()],
+#             [sg.Text('Enter something on Row 3'), sg.InputText()],
+#             [sg.Checkbox('Hello')],
+#             [sg.Button('Ok'), sg.Button('Cancel')] ]
+
+
+tab1_layout =  [[sg.T('This is inside tab 1')]]
+
+tab2_layout = [[sg.T('This is inside tab 2')],
+               [sg.In(key='in')]]
+
+layout = [[sg.TabGroup([[sg.Tab('Tab 1', tab1_layout), sg.Tab('Tab 2', tab2_layout)]])],
+              [sg.Button('Read')]]
+
+
 
 # Create the Window
 window = sg.Window('Window Title', layout)
 # Event Loop to process "events" and get the "values" of the inputs
 while True:
     event, values = window.read()
-    
-    
     
     
     if event == sg.WIN_CLOSED or event == 'Cancel': # if user closes window or clicks cancel
